84a229225ec6f337a3b4326b510e2ce5fb445fc4
Stephen Studer <stephenkstuder@gmail.com>
2020-06-07 11:43:59 -0700

    used functionality for interfaces and classes, demonstrated some ways that it will error out in the compiler.

--- greeter.js
+++ greeter.js
@@ -1,5 +1,18 @@
+/* Here we are defining a function with one argument that accepts a type of string and returns a type of string. Running this file using
+tsc.<filename>.ts will compile the code into the equivalent js code and most importantly show any errors. Some examples of possible errors
+could me that the compiler expected more arguements or a parameter or return was a different type than expected.
+*/
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
-    return "Hello, " + person;
+    return "Hello, " + person.firstName + " " + person.lastName + " ";
 }
-var user = "Jane User";
+var user = new Student("Jane", "M.", "User");
 document.body.textContent = greeter(user);
--- greeter.ts
+++ greeter.ts
@@ -1,7 +1,27 @@
-function greeter (person: string) {
-    return "Hello, " + person;
+/* Here we are defining a function with one argument that accepts a type of string and returns a type of string. Running this file using 
+tsc.<filename>.ts will compile the code into the equivalent js code and most importantly show any errors. Some examples of possible errors
+could me that the compiler expected more arguements or a parameter or return was a different type than expected. 
+*/
+
+class Student {
+    fullName: string;
+    constructor (public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
 }
 
-let user = "Jane User";
+interface Person {
+    firstName: string;
+    lastName: string;
+}
 
-// document.body.textContent = greeter(user);
+function greeter (person: Person) {
+    return `Hello, ${person.firstName} ${person.lastName} `;
+}
+
+let user = new Student ("Jane", "M.", "User");
+
+document.body.textContent = greeter(user);
+
+
+
